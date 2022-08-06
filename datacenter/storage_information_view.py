e5ec0df83808055ed7199fceb1776b3748230d01
Maxim-Pekov <64793556+Maxim-Pekov@users.noreply.github.com>
2022-08-07 01:56:48 +0300

    Moved logic in templates to view

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,11 +4,14 @@ from django.shortcuts import render
 
 def storage_information_view(request):
     non_closed_visits = Visit.objects.filter(leaved_at__isnull=True)
-    visit_durations = []
+    visits = []
     for non_closed_visit in non_closed_visits:
-        visit_durations.append({'duration': non_closed_visit.get_readably_duration()})
+        visits.append({'duration': non_closed_visit.get_readably_duration(),
+                                'owner_name': non_closed_visit.passcard.owner_name,
+                                'entered_at': non_closed_visit.entered_at,
+                                'suspect': non_closed_visit.find_long_visit()
+                       })
     context = {
-        'non_closed_visits': non_closed_visits,
-        'visit_durations': visit_durations
+        'non_closed_visits': visits
     }
     return render(request, 'storage_information.html', context)
